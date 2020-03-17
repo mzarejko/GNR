ac2a6904a189825d2be05dbb30206f98cc9ac315
szczoteczka <michalzarejko@gmail.com>
2020-03-17 09:40:46 +0100

    file reader

---
+++ Reader.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import numpy as np
+import time
+
+
+class Reader:
+
+    def __init__(self):
+        self.data_frame_int = []
+        self.data_frame_time = []
+        self.max_call_time = 1440
+
+
+    def read_file_int(self, path):
+        self.data_frame_int = []
+        if os.path.isfile(path):
+            try:
+                with open(path, 'r') as f:
+                    text = f.read()
+            except Exception as e:
+                print(str(e))
+
+            split_data = np.array(text.split())
+            self.data_frame_int = split_data.reshape(-1,2)
+
+            return self.data_frame_int
+
+    def read_file_time(self,path):
+        self.data_frame_time = []
+        if os.path.isfile(path):
+            if os.path.isfile(path):
+                try:
+                    with open(path, 'r') as f:
+                        text = f.read()
+                except Exception as e:
+                    print(str(e))
+
+                split_data = text.split()
+
+                for i in range(len(split_data)):
+                    if int(split_data[i])<self.max_call_time:
+                        self.data_frame_time.append(int(split_data[i]))
+
+
+            self.data_frame_time = np.array(self.data_frame_time).reshape(-1,1)
+            return self.data_frame_time
+
+
+
+
+
+p = Reader()
+a =p.read_file_time('time.txt')
+b = p.read_file_int('int.txt')
+print(a)
+print(b)
