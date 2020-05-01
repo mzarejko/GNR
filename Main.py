29826c2896c2fffbc1576cb03926e0fc4d3d9a1e
szczoteczka <michalzarejko@gmail.com>
2020-05-01 08:22:21 +0200

    main dodanie if

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from Data import Data
 
-d = Data()
-schedule = d.calculate_traffic_intensity()
-a, _ = d.calculate_busy_hour(schedule)
-d.create_plot(schedule, a)
+if __name__ == '__main__':
+    d = Data()
+    schedule = d.calculate_traffic_intensity()
+    a, _ = d.calculate_busy_hour(schedule)
+    d.create_plot(schedule, a)
