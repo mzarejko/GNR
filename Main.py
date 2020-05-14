8924f642bfb65803a0cfa80363bd342b4b86d94d
Konrad Pazgan <249373@student.pwr.edu.pl>
2020-05-14 11:44:36 +0200

    Dodałem printy i chyba koniec

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
+import numpy as np
+
 from Data import Data
 
 d = Data()
 schedule = d.calculate_traffic_intensity()
-a, _ = d.calculate_busy_hour(schedule)
-d.create_plot(schedule, a)
+busy_hour, busy_hour_traffic = d.calculate_busy_hour(schedule)
+d.create_plot(schedule, busy_hour)
+print("GNR: " + np.format_float_positional(busy_hour) + "min")
+print("Średnia wartość natężenia ruchu w GNR: " + np.format_float_positional(busy_hour_traffic, precision=3) + " połączenio-minut")
+input()
